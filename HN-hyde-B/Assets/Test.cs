0907f81a165c92f9d5319a6b319d31317b1a1e78
tatsuya-1970 <furutatsu1208@gmail.com>
2021-03-19 10:51:11 +0900

    test.cs変更

--- HN-hyde-B/Assets/Test.cs
+++ HN-hyde-B/Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float b;
     }
 
     // Update is called once per frame
