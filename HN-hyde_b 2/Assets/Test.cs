34d3ec610370517f4b27fd9a31ea519b1ba83359
tatsuya-1970 <furutatsu1208@gmail.com>
2021-03-22 09:56:47 +0900

    unity

---
+++ HN-hyde_b 2/Assets/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
